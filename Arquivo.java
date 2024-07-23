0ac1ddd058a2c19ca290c8b78ef7194b2cef03d5
Raiffer <144740036+Raiffer@users.noreply.github.com>
2024-07-23 19:31:02 -0300

    Create Arquivo.java

---
+++ Arquivo.java
@@ -0,0 +1,387 @@
+class Usuario{
+    private String nome, dataNascimento, email, senha;
+    private int telefone, cpf, cep;
+    
+    public boolean verificarDados(){
+        
+    }
+    
+    private String getnome(){
+        
+    }
+    
+    private int gettelefone(){
+        
+    }
+    
+    private int getcpf(){
+        
+    }
+    
+    private int getcep(){
+        
+    }
+    
+    private String getdataNascimento(){
+        
+    }
+    
+    private String getemail(){
+        
+    }
+    
+    private String getsenha(){
+        
+    }
+    
+    private void setnome(){
+        
+    }
+    
+    private void settelefone(){
+        
+    }
+    
+    private void setcpf(){
+        
+    }
+    
+    private void setcep(){
+        
+    }
+    
+    private void setdataNascimento(){
+        
+    }
+    
+    private void setemail(){
+        
+    }
+    
+    private void setsenha(){
+        
+    }
+    
+}
+
+class Organizador{
+    private int idOrganizador, orsamento, gastos, telefone, cpf, cep;
+    private String nome, dataNascimento,email, senha;
+    
+    private int getidOrganizador(){
+        
+    }
+    
+    private int getorsamento(){
+        
+    }
+    
+    private int getgastos(){
+        
+    }
+    
+    private int gettelefone(){
+        
+    }
+    
+    private int getcpf(){
+        
+    }
+    
+    private int getcep(){
+        
+    }
+    
+    private String getnome(){
+        
+    }
+    
+    private String getdataNascimento(){
+        
+    }
+    
+    private String getemail(){
+        
+    }
+    
+    private String getsenha(){
+        
+    }
+    
+    private void setidOrganizador(){
+        
+    } 
+    
+    private void setorsamento(){
+        
+    }
+    
+    private void setgastos(){
+        
+    }
+    
+    private void settelefone(){
+        
+    }
+    
+    private void setcpf(){
+        
+    }
+    
+    private void setcep(){
+        
+    }
+    
+    private void setnome(){
+        
+    }
+    
+    private void setdataNascimento(){
+        
+    }
+    
+    private void setemail(){
+        
+    }
+    
+    private void setsenha(){
+        
+    }
+    
+    private void cadastrarEvento(){
+        
+    }
+    
+    private void cancelarEvento(){
+        
+    }
+    
+    public boolean verificarLogin(){
+        
+    }
+}    
+
+class Participante{
+    
+    private int idParticipante, telefone, cpf, cep;
+    private String nome, dataNascimento, email, senha;
+    public double notaEvento;
+    
+    private int getidParticipante(){
+        
+    }
+    
+    private int gettelefone(){
+        
+    }
+    
+    private int getcpf(){
+        
+    }
+    
+    private int getcep(){
+        
+    }
+    
+    public double getnotaEvento(){
+        
+    }
+    
+    private String getnome(){
+        
+    }
+    
+    private String getdataNascimento(){
+        
+    }
+    
+    private String getemail(){
+        
+    }
+    
+    private String getsenha(){
+        
+    }
+    
+    private void setidParticipante(){
+        
+    } 
+    
+    private void setorsamento(){
+        
+    }
+    
+    private void setgastos(){
+        
+    }
+    
+    private void settelefone(){
+        
+    }
+    
+    private void setcpf(){
+        
+    }
+    
+    private void setcep(){
+        
+    }
+    
+    private void setnome(){
+        
+    }
+    
+    private void setdataNascimento(){
+        
+    }
+    
+    private void setemail(){
+        
+    }
+    
+    private void setsenha(){
+        
+    }
+    
+    private void setnotaEvento(){
+        
+    }
+    
+    
+}
+
+class Palestrante{
+    private int idPalestrante, telefone, cpf, cep;
+    private String nome, dataNascimento, email, senha;
+    
+    private int getidPalestrante(){
+        
+    }
+    
+    private int gettelefone(){
+        
+    }
+    
+    private int getcpf(){
+        
+    }
+    
+    private int getcep(){
+        
+    }
+    
+    private String getnome(){
+        
+    }
+    
+    private String getdataNascimento(){
+        
+    }
+    
+    private String getemail(){
+        
+    }
+    
+    private String getsenha(){
+        
+    }
+    
+    private void setidPalestrante(){
+        
+    } 
+    
+    private void setorsamento(){
+        
+    }
+    
+    private void setgastos(){
+        
+    }
+    
+    private void settelefone(){
+        
+    }
+    
+    private void setcpf(){
+        
+    }
+    
+    private void setcep(){
+        
+    }
+    
+    private void setnome(){
+        
+    }
+    
+    private void setdataNascimento(){
+        
+    }
+    
+    private void setemail(){
+        
+    }
+    
+    private void setsenha(){
+        
+    }
+    
+    
+}
+
+class Evento{
+    String data, nome, hora;
+    int limiteParticipante, valor;
+    
+    private String getdata(){
+        
+    }
+    
+    private String getnome(){
+        
+    }
+    
+    private String gethora(){
+        
+    }
+    
+    private int getlimiteParticipantes(){
+        
+    }
+    
+    private int getvalor(){
+        
+    }
+    
+    private void setdata(){
+        
+    }
+    
+    private void setnome(){
+        
+    }
+    
+    private void sethora(){
+        
+    }
+    
+    private void setlimiteParticipantes(){
+        
+    }
+    
+    private void setvalor(){
+        
+    }
+    
+    public void criarAtividade(){
+        
+    }
+    
+    public void removerAtividade(){
+        
+    }
+    
+    public void enviarEmail(){
+        
+    }
+}
