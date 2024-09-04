325b70c63c3c814719cd5f46428cd76724e8eac5
arthurp1910 <137120697+arthurp1910@users.noreply.github.com>
2024-09-04 13:36:52 -0300

    Add files via upload

---
+++ TrabalhoDePoo/src/sistema/view/ViewAtividadeController.java
@@ -0,0 +1,19 @@
+package sistema.view;
+
+import sistema.model.*;
+
+public class ViewAtividadeController {
+	private Model model;
+	private ViewAtividade view;
+	
+	public void initViewAtividadeController(Model model, ViewAtividade view) {
+		this.model = model;
+		this.view = view;
+		
+	}
+	
+	public void event(int caso) {
+		// Analisa os eventos do View Controller
+	}
+
+}
